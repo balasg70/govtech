80962ea6743854aa0cfe9ed8a6ed86bcc1584f44
balasg70 <balasg70@gmail.com>
2025-05-05 11:17:33 +0800

    second project

--- assigment2/deploy.sh
+++ assigment2/deploy.sh
@@ -1,18 +1,18 @@
+#!/bin/bash
 
+#Build Docker Image Locally
 
-Build Docker Image Locally
+docker build -t nodejs-mysql-crud:latest .
 
-#docker build -t nodejs-mysql-crud:latest .
+#Start Minikube and Load Image
 
-Start Minikube and Load Image
+minikube start
+minikube image load nodejs-mysql-crud:latest
 
-#minikube start
-#minikube image load nodejs-mysql-crud:latest
+#Apply Kubernetes Manifests
 
-Apply Kubernetes Manifests
-
-#kubectl apply -f mysql-pvc.yaml
-#kubectl apply -f mysql-deployment.yaml
-#kubectl apply -f deployment.yaml
-#kubectl apply -f service.yaml
-#kubectl apply -f ingress.yaml
+kubectl apply -f mysql-pvc.yaml
+kubectl apply -f mysql-deployment.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl apply -f ingress.yaml
